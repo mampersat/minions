d5c5294aa311bb55815042dbfef3773a48c8a0e1
matt <msheppard@gmail.com>
2017-01-16 21:23:45 -0500

    morse code

--- led.py
+++ led.py
@@ -1,20 +1,51 @@
 import RPi.GPIO as GPIO
 import time
-p = 17 
+p = 17
 
-def blink():
-        print "High"
+# Length of a dot/dash/etc
+dot = 0.5
+dash = dot * 3
+space = dot * 2
+word = dot * 6
+
+def dot():
+        print "."
         GPIO.output(p,GPIO.HIGH)
-        time.sleep(1)
-        print "Low"
+        time.sleep(dot)
         GPIO.output(p,GPIO.LOW)
-        time.sleep(1)
+        time.sleep(dot)
         return
 
+def dash():
+        print "-"
+        GPIO.output(p,GPIO.HIGH)
+        time.sleep(dash)
+        GPIO.output(p,GPIO.LOW)
+        time.sleep(dot)
+        return
+
+def space():
+        print " "
+        time.sleep(space)
+        return
+
+def word():
+        print "   "
+        time.sleep(word)
+        return
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(p, GPIO.OUT)
 for i in range(0,50):
-        blink()
+        dot()
+        dot()
+        dot()
+        dash()
+        dash()
+        dash()
+        dot()
+        dot()
+        dot()
+        space()
 
 GPIO.cleanup()
