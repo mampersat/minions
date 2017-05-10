d91fcae96506a5bf90da3e6c578fc056dd116732
Mampersat <msheppard@gmail.com>
2017-05-10 18:17:47 -0400

    one wire device mapping

--- esp8266/onewire/main.py
+++ esp8266/onewire/main.py
@@ -8,6 +8,11 @@ import time, ds18x20
 
 print("minion v0.1.8")
 
+names = {'ow28ff51d0c11604ce':'master_bedroom_temp',
+    'ow28ff3734c21604e0':'outside_north',
+    'ow28ff9307c11604e8':'hallway',
+}
+
 w = network.WLAN(network.STA_IF)
 # give the device a second to get connected
 time.sleep(1)
@@ -26,10 +31,15 @@ while True:
     ds.convert_temp()
     time.sleep_ms(750)
 
+    i=0
     for rom in roms:
 
         # construct sensor name containing onewire device address
-        s = 'sensor.ow' + ''.join('{:02x}'.format(x) for x in rom)
+        addr = 'ow' + ''.join('{:02x}'.format(x) for x in rom)
+        if addr in names:
+            s = 'sensor.' + names[addr]
+        else:
+            s = 'sensor.' + addr
 
         c = ds.read_temp(rom)
         f = c * (9.0/5.0) +32
@@ -48,3 +58,4 @@ while True:
         # blink integer temp value
         m = str(f).split('.')[0]
         morsecode.send(m)
+        i += 1
