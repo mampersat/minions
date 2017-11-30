89fc49389048f7e9e700772291c228c9b9a6622c
Mampersat <msheppard@gmail.com>
2017-11-30 08:39:17 -0500

    cleanup + pep8

--- esp8266/accel/main.py
+++ esp8266/accel/main.py
@@ -1,38 +1,63 @@
+""" Furnace vibration minion
+
+Measure accelerometer movement and log time the furnace is on
+"""
+
 from machine import I2C, Pin
 import time
+
+import homeassistant
 import morsecode
 
-i2c = I2C(freq=400000, scl=Pin(5), sda = Pin(4))
+SENSITIVITY = 5
+LOOP_SLEEP = 0.1
+
+# connect to the world
+hass = homeassistant.HomeAssistant('http://jarvis:8123', 'suzymatt')
+
+# create i2c object to talk to accelerometer
+i2c = I2C(freq=400000, scl=Pin(5), sda=Pin(4))
 
 # activate accelerometer
 i2c.writeto_mem(29, 0x2a, b'\x01')
 
 x = y = z = 0
 
+# are we currently vibrating
+v = False
+start = time.time()
+
+led = Pin(2, Pin.OUT)
+
 while True:
-  r = i2c.readfrom_mem(29,0x01,6)
+    # read 6 char from the accelerometer
+    r = i2c.readfrom_mem(29, 0x01, 6)
 
-  # Calculate diff
-  xd = x - r[0]
-  yd = y - r[2]
-  zd = z - r[4]
+    # Calculate diff from last readings
+    xd = x - r[0]
+    yd = y - r[2]
+    zd = z - r[4]
 
-  # Remember readings
-  x = r[0]
-  y = r[2]
-  z = r[4]
+    # Remember readings
+    x = r[0]
+    y = r[2]
+    z = r[4]
 
-  total = abs(xd) + abs(yd) + abs(zd)
+    # might need some better math her to calc 255->0 correctly
+    total = abs(xd) + abs(yd) + abs(zd)
 
-  if (total >5):
-      vibration = "XXX"
-      morsecode.send("A")
-  else:
-      vibration = ""
+    # are we vibrating?
+    if (total > SENSITIVITY):
+        # is this a new state
+        if not v:
+            v = True
+            start = time.time()
+            led.low()
+    else:
+        if v:
+            v = False
+            timespan = time.time() - start
+            print("time = ", timespan)
+            led.high()
 
-  print(total, vibration)
-
-# for i in range(0, 3):
-#    print(r[i], end="\t")
-
-  time.sleep(0.1)
+    time.sleep(LOOP_SLEEP)
