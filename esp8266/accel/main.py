8f60bdd858d464d5ab28bd2614498e455b79fc54
Mampersat <msheppard@gmail.com>
2017-12-01 19:48:46 -0500

    before solving the 255->1 problem

--- esp8266/accel/main.py
+++ esp8266/accel/main.py
@@ -9,7 +9,10 @@ import time
 import homeassistant
 import morsecode
 
-SENSITIVITY = 5
+
+print("Running")
+
+SENSITIVITY = 20 #5
 LOOP_SLEEP = 0.1
 
 # connect to the world
@@ -21,43 +24,57 @@ i2c = I2C(freq=400000, scl=Pin(5), sda=Pin(4))
 # activate accelerometer
 i2c.writeto_mem(29, 0x2a, b'\x01')
 
-x = y = z = 0
-
 # are we currently vibrating
-v = False
+recording = False
 start = time.time()
 
 led = Pin(2, Pin.OUT)
 
-while True:
-    # read 6 char from the accelerometer
-    r = i2c.readfrom_mem(29, 0x01, 6)
 
-    # Calculate diff from last readings
-    xd = x - r[0]
-    yd = y - r[2]
-    zd = z - r[4]
+def listen_for_a_sec():
+    """Ping 10 times in a second for motion, return true if detected
+    """
+    ret = False
+    x = y = z = 0
+    for t in range(0, 10):
 
-    # Remember readings
-    x = r[0]
-    y = r[2]
-    z = r[4]
+        r = i2c.readfrom_mem(29, 0x01, 6)
 
-    # might need some better math her to calc 255->0 correctly
-    total = abs(xd) + abs(yd) + abs(zd)
+        # Calculate diff from last readings
+        xd = x - r[0]
+        yd = y - r[2]
+        zd = z - r[4]
 
-    # are we vibrating?
-    if (total > SENSITIVITY):
-        # is this a new state
-        if not v:
-            v = True
-            start = time.time()
+        # Remember readings
+        x = r[0]
+        y = r[2]
+        z = r[4]
+
+        # might need some better math her to calc 255->0 correctly
+        total = abs(xd) + abs(yd) + abs(zd)
+        print(total)
+
+        # are we vibrating?
+        if (total > SENSITIVITY) and (t > 0):
             led.low()
-    else:
-        if v:
-            v = False
-            timespan = time.time() - start
-            print("time = ", timespan)
+            ret = True
+        else:
             led.high()
 
-    time.sleep(LOOP_SLEEP)
+        time.sleep(LOOP_SLEEP)
+
+    print("Listen resutl = ", ret)
+    return ret
+
+
+while True:
+    print("while true loop")
+    if listen_for_a_sec():
+        if not recording:
+            start = time.time()
+            recodring = True
+    else:
+        if recording:
+            recording = False
+            timespan = time.time() - start
+            print(timespan)
