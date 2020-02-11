27281ba3bb1bc69e7a9be3e08bc704879b799b4d
M@ <msheppard@gmail.com>
2020-02-10 21:29:00 -0500

    readings work - publishing to graphite

--- esp8266/moisture/main.py
+++ esp8266/moisture/main.py
@@ -1,10 +1,27 @@
 # Read analog pin
 
 import machine
+import network
+import socket
 import time
+
+s = network.WLAN(network.STA_IF)
+while not s.isconnected():
+    print("Network not connected - sleeping")
+    time.sleep(1)
+
+SERVER = '192.168.1.129'
+PORT = 2003
+sock = socket.socket()
+sock.connect( ( SERVER, PORT))
 
 adc = machine.ADC(0)
 
 while True:
-    print ( adc.read() )
+
+    v = adc.read()
+    t = -1
+    message = 'plant.moisture.fern %d %d\n' % (v, t )
+    sock.sendall(message)
+    print(message)
     time.sleep(1)
